56adbfc1cb9d04578aa741dd5bb2f60fff633d9f
brunoannunciato <brunobannunciato@gmail.com>
2024-12-08 02:33:57 -0300

    wip: create command to replace automatically hard values into tokens

--- src/extension.js
+++ src/extension.js
@@ -4,10 +4,38 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
 	console.log("extension called")
 	const applyTokens = vscode.commands.registerCommand('ids-toolbelt.applyIds', function () {
-		vscode.window.showInformationMessage('Tokens aplicados com sucesso!');
+		const editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			vscode.window.showErrorMessage('Nenhum arquivo ativo encontrado!');
+			return;
+		}
+		
+		const document = editor.document;
+		const fileContent = document.getText()
+
+		const updatedContent = fileContent.replace(/32px/g, 'token-ids');
+
+		if (updatedContent === fileContent) {
+			vscode.window.showInformationMessage('Nenhuma ocorrência de "32px" encontrada!');
+			return;
+		}
+
+		const edit = new vscode.WorkspaceEdit();
+		const fullRange = new vscode.Range(
+			document.positionAt(0),
+			document.positionAt(fileContent.length)
+		);
+
+		edit.replace(document.uri, fullRange, updatedContent);
+
+		vscode.workspace.applyEdit(edit).then(() => {
+			vscode.window.showInformationMessage('"32px" foi substituído por "token-ids"!');
+		});
 	});
 
 	context.subscriptions.push(applyTokens);

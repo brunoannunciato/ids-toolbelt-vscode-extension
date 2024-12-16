e0ca260d0d9bb638417322a14d69ed11711a8c00
brunoannunciato <brunobannunciato@gmail.com>
2024-12-16 12:46:14 -0300

    fix: show token value only when "rem" has just one occurency at value string

--- src/functions/showTokenValue.js
+++ src/functions/showTokenValue.js
@@ -11,8 +11,11 @@ const showTokenValue = (context) => {
                 const token = tokenVariable.replace('var(', '').replace(')', '')
                 const idsToken = tokenList[token]
 
+                const pattern = "rem"
+                const remLength = idsToken.match(new RegExp(pattern, "g") || [])?.length || 0
+
                 if (idsToken) {
-                    if (idsToken.includes('rem')) {
+                    if (remLength === 1) {
                         return new vscode.Hover(`**IDS Tokens:**\n\r\n\r **Valor do token:** *${idsToken}* \n\r **Valor convertido:** ${Number(idsToken.replace('rem', '')) * 16}px \n\r base 16px`);    
                     }
 
